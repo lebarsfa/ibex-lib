6a41ea3587e3c844a72d8a4430a669bd546f3c69
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2023-05-09 23:11:53 +0200

    Update .nupkg

--- packages/choco/ibex/tools/chocolateyuninstall.ps1
+++ packages/choco/ibex/tools/chocolateyuninstall.ps1
@@ -3,11 +3,24 @@
 # Source variables which are shared between install and uninstall.
 . $PSScriptRoot\sharedVars.ps1
 
-#Uninstall-BinFile -Name libibex.a
-#Uninstall-BinFile -Name libprim.a
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$pp = Get-PackageParameters
+$packageDir = Join-Path "$toolsDir" ".." -Resolve
+$installDir = Join-Path "$packageDir" ".." -Resolve
+if ($pp.InstallDir -or $pp.InstallationPath) { 
+	$installDir = $pp.InstallDir + $pp.InstallationPath 
+}
+Write-Host "IBEX is going to be uninstalled from '$installDir'"
+
+$root = Join-Path $installDir "ibex"
 
 if (Test-Path $CMakeRegistryPath) {
   if (Test-Path $CMakeSystemRepositoryPath\$CMakePackageName) {
       Remove-Item "$CMakeSystemRepositoryPath\$CMakePackageName"
   }
+}
+
+if (Test-Path $root) {
+    Remove-Item -Recurse -Force $root
 }
