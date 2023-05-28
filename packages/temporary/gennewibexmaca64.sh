3609ce4519e5a9c561b9062f314950502d5b1fe1
lebarsfa <fabrice.le_bars@ensta-bretagne.org>
2023-05-28 18:01:19 +0200

    Temporary scripts to be able to manually build IBEX with GAOL on specific platforms

---
+++ packages/temporary/gennewibexmaca64.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+
+mkdir -p ~/Downloads/newibex
+
+sudo pip3 uninstall -y codac ; pip3 uninstall -y codac
+sudo rm -Rf /usr/local/include/mathlib* /usr/local/include/MathLib* /usr/local/lib/libultim*
+sudo rm -Rf /usr/local/include/gaol* /usr/local/lib/gaol* /usr/local/lib/libgaol*
+sudo rm -Rf /usr/local/include/ibex* /usr/local/lib/ibex* /usr/local/lib/libibex* /usr/local/bin/ibex*
+sudo rm -Rf /usr/local/include/codac* /usr/local/lib/libcodac*
+cd ~/Downloads/newibex
+rm -Rf ibex-lib*
+git clone -b prerelease https://github.com/lebarsfa/ibex-lib
+cd ibex-lib
+export VERBOSE=1 ; mkdir build ; cd build && cmake -E env CXXFLAGS="-fPIC" CFLAGS="-fPIC" cmake -D INTERVAL_LIB=gaol -D CMAKE_OSX_DEPLOYMENT_TARGET=11.0 -D CMAKE_INSTALL_PREFIX=../ibex .. && cmake --build . --config Release --target install && cd ..
+sed -i "" "s/\\/Users\\/user\\/Downloads\\/newibex\\/ibex-lib\\/ibex/\${_IMPORT_PREFIX}/" ibex/share/ibex/cmake/*.cmake
+zip -q -r ibex_arm64_bigsur.zip ibex
+mv -f ./ibex_arm64_bigsur.zip ../
+cd ..
+
+sudo pip3 uninstall -y codac ; pip3 uninstall -y codac
+sudo rm -Rf /usr/local/include/mathlib* /usr/local/include/MathLib* /usr/local/lib/libultim*
+sudo rm -Rf /usr/local/include/gaol* /usr/local/lib/gaol* /usr/local/lib/libgaol*
+sudo rm -Rf /usr/local/include/ibex* /usr/local/lib/ibex* /usr/local/lib/libibex* /usr/local/bin/ibex*
+sudo rm -Rf /usr/local/include/codac* /usr/local/lib/libcodac*
+cd ~/Downloads/newibex
+rm -Rf ibex-lib*
+git clone -b prerelease https://github.com/lebarsfa/ibex-lib
+cd ibex-lib
+export VERBOSE=1 ; mkdir build ; cd build && cmake -E env CXXFLAGS="-fPIC" CFLAGS="-fPIC" cmake -D INTERVAL_LIB=gaol -D CMAKE_OSX_DEPLOYMENT_TARGET=12.0 -D CMAKE_INSTALL_PREFIX=../ibex .. && cmake --build . --config Release --target install && cd ..
+sed -i "" "s/\\/Users\\/user\\/Downloads\\/newibex\\/ibex-lib\\/ibex/\${_IMPORT_PREFIX}/" ibex/share/ibex/cmake/*.cmake
+zip -q -r ibex_arm64_monterey.zip ibex
+mv -f ./ibex_arm64_monterey.zip ../
+cd ..
---
+++ packages/temporary/gennewibexmsvc_x64.bat
@@ -0,0 +1,52 @@
+@cd /d "%~dp0"
+
+@echo off
+
+rem choco install -y -r --no-progress wget zip winflexbison patch
+rem sed
+
+rd /s /q "%ProgramFiles%\mathlib" "%ProgramFiles(x86)%\mathlib" "%ProgramFiles%\gaol" "%ProgramFiles(x86)%\gaol" "%ProgramFiles%\IBEX" "%ProgramFiles(x86)%\IBEX"
+cd P:\devel\GitHub
+
+rd /s /q mathlib_build_x64_vc15
+md mathlib_build_x64_vc15
+cd mathlib_build_x64_vc15
+cmake -G "Visual Studio 15" -A x64 ..\mathlib
+cmake --build . --config Release --target install
+cd ..
+
+rd /s /q gaol_build_x64_vc15
+md gaol_build_x64_vc15
+cd gaol_build_x64_vc15
+cmake -G "Visual Studio 15" -A x64 ..\gaol
+cmake --build . --config Release --target install
+cd ..
+
+rd /s /q ibex-lib_build_x64_vc15
+md ibex-lib_build_x64_vc15
+cd ibex-lib_build_x64_vc15
+cmake -E env CXXFLAGS=" /wd4267 /wd4244 /wd4305 /wd4996" CFLAGS=" /wd4267 /wd4244 /wd4305 /wd4996" cmake -G "Visual Studio 15" -A x64 -D INTERVAL_LIB=gaol -D MATHLIB_DIR="%ProgramFiles%\\mathlib" -D GAOL_DIR="%ProgramFiles%\\gaol" ..\ibex-lib
+cmake --build . --config Release --target install
+cd ..
+
+md "%ProgramFiles%\IBEX\include\ibex\3rd\gaol" "%ProgramFiles%\IBEX\lib\ibex\3rd"
+copy "%ProgramFiles%\mathlib\include\*" "%ProgramFiles%\IBEX\include\ibex\3rd"
+copy "%ProgramFiles%\gaol\include\gaol" "%ProgramFiles%\IBEX\include\ibex\3rd\gaol"
+copy "%ProgramFiles%\mathlib\lib\*" "%ProgramFiles%\IBEX\lib\ibex\3rd"
+copy "%ProgramFiles%\gaol\lib\*" "%ProgramFiles%\IBEX\lib\ibex\3rd"
+
+rem echo Might need to run manually the rest...
+rem pause
+
+rem Due to sed error message mentionning cross-device link...
+cd /d C:
+
+sed -i "s/C:\\/Program Files\\/mathlib\\/lib/\${_IMPORT_PREFIX}\\/lib\\/ibex\\/3rd/" "%ProgramFiles%\IBEX\share\ibex\cmake\ibex-config-ultim.cmake"
+sed -i "s/C:\\/Program Files\\/mathlib\\/include/\${_IMPORT_PREFIX}\\/include\\/ibex\\/3rd/" "%ProgramFiles%\IBEX\share\ibex\cmake\ibex-config-ultim.cmake"
+sed -i "s/C:\\/Program Files\\/gaol\\/lib/\${_IMPORT_PREFIX}\\/lib\\/ibex\\/3rd/" "%ProgramFiles%\IBEX\share\ibex\cmake\ibex-config-gaol.cmake"
+sed -i "s/C:\\/Program Files\\/gaol\\/include/\${_IMPORT_PREFIX}\\/include\\/ibex\\/3rd/" "%ProgramFiles%\IBEX\share\ibex\cmake\ibex-config-gaol.cmake"
+
+cd "%ProgramFiles%"
+zip -q -r ibex_x64_vc15.zip ibex
+
+pause
